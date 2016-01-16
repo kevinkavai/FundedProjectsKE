02e043e047a16089af52f23ea809f693a9ed2474
Allan <aonchuru@strathmore.edu>
2016-01-16 16:15:39 +0300

    Fix GeoJSON coordinates format to display markers in correct locations

--- py/coordslist.py
+++ py/coordslist.py
@@ -4,9 +4,10 @@
 
 def get_coordinates(coordinates_string):
     if not coordinates_string or coordinates_string == '0' or coordinates_string == 0:
-        coordinates_string = "(-0.0, 0.0)"  # How else?
+        coordinates_string = "(-0.0, 0.0)"  # How else not to include these missing coordinates?
 
-    latlong_list = coordinates_string[1:-1].split(',')  # Remove brackets and put in list
+    latlong_list = coordinates_string[1:-1].split(',')  # Remove brackets and put lat and long in list
+    latlong_list.reverse()  # reverse into format [long, lat]
     return [float(i) for i in latlong_list]  # Convert to floating point numbers
 
 # Test function
